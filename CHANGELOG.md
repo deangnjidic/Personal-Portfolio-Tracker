862fb6ace8dbe3b13aeb87c672ac0ecc59cf4e76
Dean Gnjidic <22212870+deangnjidic@users.noreply.github.com>
2026-01-30 15:16:02 +0000

    v1.15.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ---
 
+## [1.15.1] - 2026-01-30
+
+### Fixed
+- Cookie consent "Accept" button now works correctly (fixed gtag function scope issue)
+- Google Analytics and GTM now load properly after accepting cookies
+
+---
+
 ## [1.15.0] - 2026-01-29
 
 ### Added
--- cookie-consent.js
+++ cookie-consent.js
@@ -24,13 +24,19 @@
         window.dataLayer = window.dataLayer || [];
         
         // Define gtag function globally
-        window.gtag = function(){dataLayer.push(arguments);}
+        window.gtag = function(){
+            window.dataLayer.push(arguments);
+        };
         
         // Google Tag Manager
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-PWHJ9WLH');
 
         // Google Analytics 4
